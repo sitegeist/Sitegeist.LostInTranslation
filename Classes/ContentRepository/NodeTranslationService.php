7a1b836285e80b59fe860305956a68c4a718d892
Felix Gradinaru <felix@gradinaru.me>
2023-09-12 16:57:32 +0200

    BUGFIX: Delete nodes properly, avoid source node not being found

--- Classes/ContentRepository/NodeTranslationService.php
+++ Classes/ContentRepository/NodeTranslationService.php
@@ -184,7 +184,7 @@ class NodeTranslationService
             return;
         }
 
-        $this->nodesToBeTranslated[$workspace->getName()][$nodeSourceDimensionValue][$node->getIdentifier()] = $node->getIdentifier();
+        $this->nodesToBeTranslated[$workspace->getName()][$nodeSourceDimensionValue][$node->getIdentifier()] = $node;
     }
 
     /**
@@ -208,8 +208,7 @@ class NodeTranslationService
                  * @var string $nodeIdentifier
                  * @var bool $translate
                  */
-                foreach ($nodesByLanguageDimensionValue as $nodeIdentifier) {
-                    $node = $context->getNodeByIdentifier($nodeIdentifier);
+                foreach ($nodesByLanguageDimensionValue as $nodeIdentifier => $node) {
                     if ($this->skipAuthorizationChecks) {
                         $this->securityContext->withoutAuthorizationChecks(function () use ($node, $workspaceName) {
                             $this->syncNode($node, $workspaceName);
