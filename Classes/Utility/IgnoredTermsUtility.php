a3d1c57e15031880fe3d112f81a70084f44b898f
Felix Gradinaru <felix@gradinaru.me>
2024-01-16 23:56:42 +0100

    TASK: Make PHPStan happy and prevent potential null case

--- Classes/Utility/IgnoredTermsUtility.php
+++ Classes/Utility/IgnoredTermsUtility.php
@@ -6,13 +6,21 @@ namespace Sitegeist\LostInTranslation\Utility;
 
 class IgnoredTermsUtility
 {
+    /**
+     * @param string $string
+     * @param string[]  $ignoredTerms
+     *
+     * @return string
+     */
     public static function wrapIgnoredTerms(string $string, array $ignoredTerms): string
     {
-        return preg_replace('/(' . implode('|', $ignoredTerms) . ')/i', '<ignore>$1</ignore>', $string);
+        $stringWithWrappedIgnoredTerms = preg_replace('/(' . implode('|', $ignoredTerms) . ')/i', '<ignore>$1</ignore>', $string);
+        return !is_null($stringWithWrappedIgnoredTerms) ? $stringWithWrappedIgnoredTerms : $string;
     }
 
     public static function unwrapIgnoredTerms(string $string): string
     {
-        return preg_replace('/(<ignore>|<\/ignore>)/i', '', $string);
+        $stringWithUnwrappedIgnoredTerms = preg_replace('/(<ignore>|<\/ignore>)/i', '', $string);
+        return !is_null($stringWithUnwrappedIgnoredTerms) ? $stringWithUnwrappedIgnoredTerms : $string;
     }
 }
