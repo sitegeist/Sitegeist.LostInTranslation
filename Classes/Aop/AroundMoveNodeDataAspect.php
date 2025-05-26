34f695f3d2a86319eefa610171eaf0dcae0e0f4b
Felix Gradinaru <felix@gradinaru.me>
2025-05-26 14:44:54 +0200

    FEATURE: Add aspect to avoid duplicate-moving of nodes

---
+++ Classes/Aop/AroundMoveNodeDataAspect.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Sitegeist\LostInTranslation\Aop;
+
+use Neos\ContentRepository\Domain\Model\NodeData;
+use Neos\Flow\Annotations as Flow;
+use Neos\Flow\Aop\JoinPointInterface;
+use Neos\Utility\Arrays;
+use Sitegeist\LostInTranslation\ContentRepository\NodeTranslationService;
+
+/**
+ * @Flow\Aspect
+ */
+class AroundMoveNodeDataAspect
+{
+    /**
+     * @Flow\Inject
+     * @var NodeTranslationService
+     */
+    protected $nodeTranslationService;
+
+    /**
+     * @Flow\InjectConfiguration(path="nodeTranslation.languageDimensionName")
+     * @var string
+     */
+    protected $languageDimensionName;
+
+    /**
+     * @Flow\InjectConfiguration(package="Neos.ContentRepository", path="contentDimensions")
+     * @var array
+     */
+    protected $contentDimensionConfiguration;
+
+    /**
+     * @param  JoinPointInterface  $joinPoint
+     * @return mixed
+     *
+     * @Flow\Around("method(Neos\ContentRepository\Domain\Model\Node->moveNodeData())")
+     */
+    public function aroundMoveNodeData(JoinPointInterface $joinPoint): mixed
+    {
+        if (FLOW_SAPITYPE === 'CLI') {
+            return $joinPoint->getAdviceChain()->proceed($joinPoint);
+        }
+
+        /** @var NodeData $nodeData */
+        $nodeData = $joinPoint->getMethodArgument('nodeData');
+        $configuration = $this->contentDimensionConfiguration[$this->languageDimensionName];
+
+        $dimensionValues = $nodeData->getDimensionValues();
+        $nodeDataLanguageDimensionValue = Arrays::getValueByPath($dimensionValues, $this->languageDimensionName . '.0');
+
+        if (
+            !is_null($nodeDataLanguageDimensionValue) && !$this->nodeTranslationService->isActive() && Arrays::getValueByPath(
+                $configuration,
+                sprintf('presets.%s.options.translationStrategy', $nodeDataLanguageDimensionValue)
+            ) === NodeTranslationService::TRANSLATION_STRATEGY_SYNC
+        ) {
+            return null;
+        }
+
+        return $joinPoint->getAdviceChain()->proceed($joinPoint);
+    }
+}
