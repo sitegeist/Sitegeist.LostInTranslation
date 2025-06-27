d0f545c5862e641074288ff6f316fee9e3a03555
Martin Ficzel <ficzel@sitegeist.de>
2025-06-27 14:12:40 +0200

    TASK: Remove ununsed use statements

--- Classes/Infrastructure/DeepL/DeepLCacheIdentifierFactory.php
+++ Classes/Infrastructure/DeepL/DeepLCacheIdentifierFactory.php
@@ -4,9 +4,6 @@ declare(strict_types=1);
 
 namespace Sitegeist\LostInTranslation\Infrastructure\DeepL;
 
-use Neos\Cache\Frontend\StringFrontend;
-use Neos\Flow\Annotations as Flow;
-
 class DeepLCacheIdentifierFactory
 {
     public function createEntryIdentifier(string $sourceText, ?string $sourceLanguage, string $targetLanguage): string
