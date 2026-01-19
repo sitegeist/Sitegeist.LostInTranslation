0bf1f8968d292643e2eca8a54dc9647b3ac5f0d8
Daniel Lienert <daniel@lienert.cc>
2026-01-19 11:36:00 +0100

    BUGFIX: Handle uri path segement separately

--- Classes/ContentRepository/CommandHook/TranslationCommandHook.php
+++ Classes/ContentRepository/CommandHook/TranslationCommandHook.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Sitegeist\LostInTranslation\ContentRepository\CommandHook;
 
+use Behat\Transliterator\Transliterator;
 use Neos\ContentRepository\Core\CommandHandler\CommandHookInterface;
 use Neos\ContentRepository\Core\CommandHandler\CommandInterface;
 use Neos\ContentRepository\Core\CommandHandler\Commands;
@@ -116,6 +117,8 @@ final class TranslationCommandHook implements CommandHookInterface
             return Commands::createEmpty();
         }
 
+        $translatedProperties = $this->handleSpecialProperties($translatedProperties);
+
         $newCommand = SetNodeProperties::create(
             $command->workspaceName,
             $command->nodeAggregateId,
@@ -124,5 +127,18 @@ final class TranslationCommandHook implements CommandHookInterface
         );
 
         return Commands::fromArray([$newCommand]);
+    }
+
+    /**
+     * @param array<string,string> $translatedProperties
+     * @return array<string,string>
+     */
+    private function handleSpecialProperties(array $translatedProperties): array
+    {
+        if (!empty($translatedProperties['uriPathSegment'])) {
+            $translatedProperties['uriPathSegment'] = Transliterator::urlize($translatedProperties['uriPathSegment']);
+        }
+
+        return $translatedProperties;
     }
 }
